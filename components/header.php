b44187abe9a51260529d3c5e2686554bceea6189
cerneigabriel <40870657+cerneigabriel@users.noreply.github.com>
2020-07-01 13:05:55 +0300

    setup

---
+++ components/header.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="UTF-8" />
+    <link rel="shortcut icon" type="image/x-icon" href="" />
+    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
+    <meta name="theme-color" content="#000000" />
+    <link rel="manifest" href="./manifest.json" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta property="og:image" content="https://127.0.0.1:5500/images/escoala.jpg" />
+    <meta property="og:image:url" content="https://127.0.0.1:5500/images/escoala.jpg" />
+    <meta property="og:url" content="https://127.0.0.1:5500/" />
+    <meta name="application-name" content="eGradinita" />
+    <meta property="og:title" content="eGradinita - Admitere online în clasa I" />
+    <meta property="og:description" content="Depunerea cererii de înmatriculare a copilului în clasa I în instituțiile de învățământ a mun. Chișinău" />
+    <meta property="description" content="Depunerea cererii de înmatriculare a copilului în clasa I în instituțiile de învățământ a mun. Chișinău" />
+
+    <title>eGradinita</title>
+
+    <link rel="stylesheet" href="./css/main.css" />
+</head>
+
+<body>
+
+    <!-- Header -->
+    <header class="Header Header--container">
+        <div class="container">
+            <div class="">
+                <a href="/" class="header-logo">
+                    <img src="./images/logo.svg" alt="" />
+                </a>
+            </div>
+            <div class="">
+                <a href="https://www.chisinau.md/" target="_blank">
+                    <img src="./images/primaria_logo.svg" alt="chisinau-logo" draggable="false" />
+                </a>
+            </div>
+        </div>
+    </header>
+    <!-- Header End -->
+
+    <!-- Navbar -->
+    <div class="Navbar Navbar--container">
+        <div class="container">
+            <ul class="Navbar__nav">
+                <li class="Navbar__item">
+                    <a href="./pages/schools.html" class="Navbar__link">Scoli</a>
+                </li>
+                <li class="Navbar__item Dropdown">
+                    <a href="./pages/info.html" class="Navbar__link Dropdown__button">
+                        Informatii
+                    </a>
+                    <ul class="Dropdown__menu">
+                        <li class="Dropdown__item">
+                            <a href="https://docs.google.com/document/d/1qBRxy3xZzkq33RcZb8ynJAC7hdVgh01GvKRmuMzlvOw" target="_blank" class="Dropdown__link">
+                                Ghid de aplicare
+                            </a>
+                        </li>
+                        <li class="Dropdown__item">
+                            <a href="./pages/rules.html" class="Dropdown__link">
+                                Regulament
+                            </a>
+                        </li>
+                        <li class="Dropdown__item">
+                            <a href="./pages/faq.html" class="Dropdown__link">
+                                Întrebări frecvente
+                            </a>
+                        </li>
+                        <li class="Dropdown__item">
+                            <a href="./pages/viguide.html" class="Dropdown__link">
+                                Instrucțiuni video
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="Navbar__item">
+                    <a href="./pages/assignments.html" class="Navbar__link">Cereri</a>
+                </li>
+            </ul>
+            <ul class="Navbar__nav ml-auto">
+                <li class="Navbar__item">
+                    <button type="button" class="Navbar__link Button Button__secondary Button--sm Button--badge Button__secondary--active">
+                        Clasa I-a
+                    </button>
+                    <button type="button" class="Navbar__link Button Button__secondary Button--sm Button--badge">
+                        Clasa X-a
+                    </button>
+                </li>
+                <li class="Navbar__item">
+                    <button type="button" class="Navbar__link Link Link__primary Link__primary--active mr-2">
+                        RO
+                    </button>
+                    <button type="button" class="Navbar__link Link Link__primary">
+                        РУ
+                    </button>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!-- Navbar End -->
---
+++ index.php
@@ -0,0 +1,96 @@
+<?php include 'components/header.php'; ?>
+
+<!-- Jumbotron -->
+<div class="Jumbotron" style="
+        background: url(../images/girl.jpg) center center / cover no-repeat;
+      ">
+    <div class="Jumbotron__content Jumbotron__content--right text--white">
+        <div class="container">
+            <div class="Display--1">Înscriere în clasa I</div>
+            <div class="Display--2 text--yellow text--upper">
+                Simplu și transparent
+            </div>
+            <div class="row flex-column">
+                <div class="Jumbotron__description ml-auto col-lg-6">
+                    <b>Etapa I</b> 21 mai, ora 8:00 - 19 iunie, ora 17:00 - Depunerea
+                    cererilor în instituții conform adresei de domiciliu.
+                </div>
+                <div class="Jumbotron__description ml-auto col-lg-6">
+                    <b>Etapa II</b> 22 iunie, ora 8:00 - 28 august, ora 17:00 -
+                    Completarea locurilor rămase disponibile.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Jumbotron -->
+
+<!-- Jumbotron -->
+<div class="Jumbotron py-5 m-0" style="
+        background: url(../images/teens.jpg) center center / cover no-repeat;
+      ">
+    <div class="Jumbotron__content Jumbotron__content--right text--white">
+        <div class="container">
+            <div class="Display--1">Admitere in clasa X</div>
+            <p class="Jumbotron__description m-0">
+                <b>Etapa I:</b> 22 iunie – 10 iulie<br />
+                <b>Etapa II:</b> 10 august - 18 august
+            </p>
+        </div>
+    </div>
+</div>
+<!-- Jumbotron -->
+
+<!-- Jumbotron -->
+<div class="Jumbotron" style="padding: 170px 0;">
+    <div class="Jumbotron__content Jumbotron__content--center">
+        <div class="container">
+            <div class="Step">
+                <div class="Step__item">
+                    <div class="Step__icon">
+                        <img src="./images/steps/step_1.svg" alt="" />
+                    </div>
+                    <h3 class="Step__title">Pasul 1</h3>
+                    <p class="Step__description">Autorizați-vă</p>
+                </div>
+                <div class="Step__item">
+                    <div class="Step__icon">
+                        <img src="./images/steps/step_2.svg" alt="" />
+                    </div>
+                    <h3 class="Step__title">Pasul 2</h3>
+                    <p class="Step__description">Alegeți instituția</p>
+                </div>
+                <div class="Step__item">
+                    <div class="Step__icon">
+                        <img src="./images/steps/step_3.svg" alt="" />
+                    </div>
+                    <h3 class="Step__title">Pasul 3</h3>
+                    <p class="Step__description">Aplicați cererea</p>
+                </div>
+                <div class="Step__item">
+                    <div class="Step__icon">
+                        <img src="./images/steps/step_4.svg" alt="" />
+                    </div>
+                    <h3 class="Step__title">Pasul 4</h3>
+                    <p class="Step__description">Verificați rezultatele</p>
+                </div>
+                <div class="Step__item">
+                    <div class="Step__icon">
+                        <img src="./images/steps/step_5.svg" alt="" />
+                    </div>
+                    <h3 class="Step__title">Pasul 5</h3>
+                    <p class="Step__description">Prezentați-vă la instituție</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Jumbotron -->
+
+<!-- Jumbotron -->
+<div class="Jumbotron bg--grey" style="padding: 80px 0;">
+    <div class="Jumbotron__content Jumbotron__content--center"></div>
+</div>
+<!-- Jumbotron -->
+
+<?php include 'components/footer.php' ?>
